dd99d2c932877e25069a9ad10c8683394e2b001c
PHILIP ALEX AMBATTU <ambatttuphilipalex@gmail.com>
2026-06-26 18:01:20 +0200

    Ball Frame Added

--- data/frame_reader.py
+++ data/frame_reader.py
@@ -2,10 +2,12 @@ import cv2
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
+import json
+from torch.utils.data import Dataset
 
 
 VIDEO_PATH = "/Users/philipalexambattu/Documents/DL Lab Final/Table Tennis Referee/datasets/OpenTT/videos/train/game_1.mp4"
-
+BALL_JSON = "/Users/philipalexambattu/Documents/DL Lab Final/Table Tennis Referee/datasets/OpenTT/annotations/train/game_1_ball.json"
 
 def read_frame(frame_no):
     cur = cv2.VideoCapture(VIDEO_PATH)
@@ -20,6 +22,48 @@ def read_frame(frame_no):
     frame = frame.astype("float32")
     frame = frame / 255
     frame = np.transpose(frame, axes=(2,0,1))
-    frame = torch.from_numpy(frame)
-    print(frame.shape, frame.min(), frame.max())
+    frame = torch.tensor(frame)    
     return frame
+
+
+class BallFrameDataset(Dataset):
+    
+    def __init__(self):
+        cur = cv2.VideoCapture(VIDEO_PATH)
+        height = cur.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        width = cur.get(cv2.CAP_PROP_FRAME_WIDTH)
+        self.height = int(height)
+        self.width = int(width)
+        cur.release()
+        
+        with open(BALL_JSON) as f:
+            ball_json = json.load(f)
+        self.items = []
+        for key,val in ball_json.items():
+            if val['x'] == -1 and val['y'] == -1:
+                continue
+            nx = val['x']/self.width
+            ny = val['y']/self.height
+            self.items.append((int(key), nx, ny))                
+        
+    def __len__(self):
+        return len(self.items)
+    
+    def __getitem__(self,i):
+        selected = self.items[i]
+        frame_no = selected[0]
+        nx = selected[1]
+        ny = selected[2]
+        frame = read_frame(frame_no)        
+        ball_co_ordinates = torch.tensor([nx,ny],dtype=torch.float32)
+        return frame, ball_co_ordinates
+
+
+if __name__ == "__main__":
+    
+    d = BallFrameDataset()
+    print(len(d))
+    frame, ball = d[0]
+    print(frame.shape, ball)
+    
+    
